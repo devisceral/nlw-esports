529a8ed80d7214015b07212c92063847461d4909
André Mendes <develmendes@gmail.com>
2022-09-15 17:14:09 -0300

    'tailwind'

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,8 @@
 
-interface ButtonProps{
-  title?: string;
-}
-
-function Button(props: ButtonProps){
-  return (
-    <button>{props.title}</button>
-  )
-}
-
+import './styles/main.css';
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Button title="send1" />
-      <Button title="send2" />
-      <Button title="send3" />
+    <div className="w-8 h-8 bg-violet-500">
     </div>
   )
 }
